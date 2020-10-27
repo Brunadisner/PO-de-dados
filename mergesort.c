ba5bb8972f4e87ef2f5aaee7e1ec262f8c72b839
Brunadisner <bruna.disner@gmail.com>
2020-10-27 11:18:08 -0300

    merge com erro

--- mergesort.c
+++ mergesort.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void mergesort (int *vet, int inicio, int fim) {
     if (inicio < fim) {
-        int meio = intercala((inicio + fim)/2);
+        int meio =floor(((inicio + fim)/2));
         mergesort(vet, inicio, meio);
         mergesort(vet, meio+1, fim);
         intercala(vet, inicio, meio, fim);
@@ -12,39 +13,43 @@ void mergesort (int *vet, int inicio, int fim) {
 }
 
 void intercala (int *vet, int inicio, int meio, int fim) {
-    int aux = malloc (fim - inicio + 1);
+    int aux = fim - inicio + 1;
     int i = inicio;
     int j = meio + 1;
-    int *k = 0;
+    int p1 = 0, p2 = 0;
+    int *k;
+    k = (int *) malloc(aux*sizeof(int));
 
-    while (i <= meio && j <= fim) {
-        if (vet[i] <= vet[j]) {
-            aux[k] = vet[i];
-            i++;
-        } else {
-            aux[k] = vet[j];
-            j++;
+    if(k != NULL){  
+        for(int x = 0; x<aux; x++){
+            if (!p1 && !p2) {
+                if(vet[i] < vet[j]){
+                    k[x] = vet[i++];
+                }
+                else{
+                    k[x] = vet[j++];
+                }
+                if(i>meio) p1=1;
+                if(j>fim) p2=1;
+            }
+            else{
+                if(!p1){
+                    k[x] = vet[i++];
+                }else{  
+                    k[x] = vet[j++];
+                }
+            }
+        
+    
         }
-        k++;
-    }
 
-    while (i <= meio) {
-        aux[k] = vet[i];
-        k++;
-        i++;
-    }
-
-    while (j <= fim) {
-        aux[k] = vet[j];
-        k++;
-        j++;
-    }
-
-    for (k = inicio; k <= fim; k++) {
-        vet[*k] = aux[k - inicio];
-    }
-    free(aux);
+for (int aux = inicio; aux <= fim; aux++) {
+        vet[aux] = k[aux - inicio];
 }
+}
+free(k);
+}
+
 
 void main(){
     int vet[5] = {3,27,6,4,5};
