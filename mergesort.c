11e8284a03174d5037fe01526f9f46829ef452ab
Brunadisner <bruna.disner@gmail.com>
2020-10-27 12:19:15 -0300

    correção do merge

--- mergesort.c
+++ mergesort.c
@@ -3,6 +3,28 @@
 #include <string.h>
 #include <math.h>
 
+void mergesort (int *vet, int inicio, int fim);
+void intercala (int *vet, int inicio, int meio, int fim);
+
+
+void main(){
+    int vet[5] = {3,27,6,4,5};
+    int n=5;
+
+    printf("Valore desordenados ");
+    for(int q=0;q<n;q++){
+        printf("%d ",vet[q]);
+        }
+    printf("\n");
+
+    mergesort(vet, 0, n);
+
+    printf("Valore ordenados ");
+    for(int q=0;q<n;q++){
+        printf("%d ",vet[q]);
+        }
+    printf("\n");
+}
 void mergesort (int *vet, int inicio, int fim) {
     if (inicio < fim) {
         int meio =floor(((inicio + fim)/2));
@@ -11,7 +33,6 @@ void mergesort (int *vet, int inicio, int fim) {
         intercala(vet, inicio, meio, fim);
     }
 }
-
 void intercala (int *vet, int inicio, int meio, int fim) {
     int aux = fim - inicio + 1;
     int i = inicio;
@@ -48,24 +69,4 @@ for (int aux = inicio; aux <= fim; aux++) {
 }
 }
 free(k);
-}
-
-
-void main(){
-    int vet[5] = {3,27,6,4,5};
-    int n=5;
-
-    printf("Valore desordenados ");
-    for(int q=0;q<n;q++){
-        printf("%d ",vet[q]);
-        }
-    printf("\n");
-
-    mergesort(vet, 0, n);
-
-    printf("Valore ordenados ");
-    for(int q=0;q<n;q++){
-        printf("%d ",vet[q]);
-        }
-    printf("\n");
 }
