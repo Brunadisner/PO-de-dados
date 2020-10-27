f83711a94dc12de2ca7a5861788295f575d15208
Brunadisner <bruna.disner@gmail.com>
2020-10-27 09:58:42 -0300

    add merge e quick sort

---
+++ quicksort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(int x, int y){ 
+   int temp;
+   temp = x;
+   x = y;
+   y = temp;
+}
+
+int particiona (int *vet, int inicio, int fim) {
+    int pivo = fim;
+    int cont = inicio;
+    for (int i = inicio; i < fim; i++){
+        if(vet[i] <= vet[pivo]) {
+            cont++;
+            /*pivo = vet[i];
+            vet[i] = vet[inicio];
+            vet[inicio] = pivo;*/
+            swap(vet[cont], vet[i]);
+        }
+    }
+
+    if (vet[cont] > vet[pivo]){
+        /*pivo = vet[inicio];
+        vet[inicio] = vet[cont];
+        vet[cont] = pivo;*/
+        cont++;
+        swap(vet[cont], vet[pivo]);
+    }
+    
+    return pivo;
+}
+
+void quicksort (int *vet, int inicio, int fim) {
+    int pivo;
+    if (inicio < fim) {
+        pivo = particiona (vet, inicio, fim);
+        quicksort(vet, inicio, pivo-1);
+        quicksort(vet, pivo+1, fim);
+    }
+}
+
+
+void main(){
+    int vet[5] = {3,27,6,4,5};
+    int n=5;
+
+    printf("Valore desordenados ");
+    for(int q=0;q<n;q++){
+        printf("%d ",vet[q]);
+        }
+    printf("\n");
+
+    quicksort(vet, 0, n);
+
+    printf("Valore ordenados ");
+    for(int q=0;q<n;q++){
+        printf("%d ",vet[q]);
+        }
+    printf("\n");
+}
