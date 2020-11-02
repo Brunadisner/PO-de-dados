c3859d52b7c5a2a131da4e8abfeeaf2e594f81a0
Brunadisner <bruna.disner@gmail.com>
2020-11-01 21:39:39 -0300

    arrumando o quick

--- quicksort.c
+++ quicksort.c
@@ -8,7 +8,7 @@ int particiona (int *vet, int inicio, int fim);
 int particiona (int *vet, int inicio, int fim) {
     int pivo = vet[inicio];
     int aux, esq, dir;
-    esq = inicio;
+    esq = inicio + 1;
     dir = fim;
     while(esq<dir){
         while(vet[esq]<=pivo){
@@ -32,17 +32,20 @@ int particiona (int *vet, int inicio, int fim) {
 
 void quicksort (int *vet, int inicio, int fim) {
     int pivo;
+    printf("Inicio %d \n fim %d \n",inicio,fim);
     if (inicio < fim) {
         pivo = particiona (vet, inicio, fim);
+        printf("pivo %d\n ",pivo);
         quicksort(vet, inicio, pivo-1);
         quicksort(vet, pivo+1, fim);
+
     }
 }
 
 
 void main(){
-    int vet[9] = {8,-7,15,10,15,-2,0,100,3};
-    int n=9;
+    int vet[12] = {15,5,4,3,0,2,6,11,-1,10,7,8};
+    int n=12;
 
     printf("Valore desordenados ");
     for(int q=0;q<n;q++){
