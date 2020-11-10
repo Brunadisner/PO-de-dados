0e5b6e112de5e8a1f1c4c090e75bd161e9f857fc
Brunadisner <bruna.disner@gmail.com>
2020-11-10 20:48:59 -0300

    add radix sort

--- quicksort.c
+++ quicksort.c
@@ -10,17 +10,19 @@ int particiona (int *vet, int inicio, int fim) {
     int aux, esq, dir;
     esq = inicio + 1;
     dir = fim;
-    while(esq<dir){
-        while(vet[esq]<=pivo){
+    while(esq <= dir){
+        if(vet[esq] <= pivo){
             esq++;
         }
-        while (vet[dir] > pivo){
+        else if (vet[dir] > pivo){
             dir--;
         }
-        if(esq < dir){
+        else if(esq <= dir){
             aux = vet[esq];
             vet[esq] = vet[dir];
             vet[dir] = aux;
+            esq++;
+            dir--;
         }
         
         
@@ -35,7 +37,6 @@ void quicksort (int *vet, int inicio, int fim) {
     printf("Inicio %d \n fim %d \n",inicio,fim);
     if (inicio < fim) {
         pivo = particiona (vet, inicio, fim);
-        printf("pivo %d\n ",pivo);
         quicksort(vet, inicio, pivo-1);
         quicksort(vet, pivo+1, fim);
 
@@ -44,8 +45,8 @@ void quicksort (int *vet, int inicio, int fim) {
 
 
 void main(){
-    int vet[12] = {15,5,4,3,0,2,6,11,-1,10,7,8};
-    int n=12;
+    int vet[13] = {15,5,4,3,0,2,6,11,-1,10,7,8,-3};
+    int n=13;
 
     printf("Valore desordenados ");
     for(int q=0;q<n;q++){
@@ -53,7 +54,7 @@ void main(){
         }
     printf("\n");
 
-    quicksort(vet, 0, n);
+    quicksort(vet, 0, n-1);
 
     printf("Valore ordenados ");
     for(int q=0;q<n;q++){
